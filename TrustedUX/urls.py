f412f4dd3d3cba9213ecfbb459fa916a30a20c2a
Pankaj chejara <pankajch@tlu.ee>
2021-05-13 17:03:23 +0300

    update 404 page

--- TrustedUX/urls.py
+++ TrustedUX/urls.py
@@ -24,7 +24,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls import handler404, handler500
+
 handler404 = sv.error_404
+handler500 = sv.error_404
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
