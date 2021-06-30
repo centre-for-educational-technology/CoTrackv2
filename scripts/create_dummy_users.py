c7cc5e74e5deb5244842cb69b1b1b0313ae1d3cb
Pankaj chejara <pankajch@tlu.ee>
2021-06-30 10:18:18 +0300

    extensions

---
+++ scripts/create_dummy_users.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+def run():
+    for i in range(10):
+        user_name = 'user' + str(i)
+        user_email = 'user' + str(i) + '@demo.ee'
+        user_pwd = 'abc1234#'
+        first_name = user_name
+        last_name = 'demo'
+        try:
+            obj = User.objects.create(first_name=first_name,last_name=last_name,username=user_name,email=user_email,password=user_pwd)
+        except:
+            print('error occurred.')
