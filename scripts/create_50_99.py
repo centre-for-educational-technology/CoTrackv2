9f343ab60b5565275d97775a2d538a2d78e79e8f
Pankaj Chejara <pankajchejara23@gmail.com>
2021-07-12 11:52:03 +0300

    add 50 more users

---
+++ scripts/create_50_99.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+def run():
+    for i in range(50):
+        user_name = 'user' + str( 50 + i)
+        user_email = 'user' + str(50 + i) + '@demo.ee'
+        user_pwd = make_password("abc1234#")
+        first_name = user_name
+        last_name = 'demo'
+        try:
+            obj = User.objects.create(first_name=first_name,last_name=last_name,username=user_name,email=user_email,password=user_pwd)
+        except:
+            print('error occurred.')
