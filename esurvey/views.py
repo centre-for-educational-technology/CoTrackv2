36e5b372ef303cc5c7bfd66d2ec0c92c5a0aa237
Pankaj Chejara <pankajchejara23@gmail.com>
2022-04-02 20:22:47 +0200

    using tz_convert to helsinki and then localize

--- esurvey/views.py
+++ esurvey/views.py
@@ -1340,6 +1340,7 @@ def getVadDf(session_id,group_id):
         vad_df =vad_df.append({'timestamp':v.timestamp,'user':v.user.authormap.authorid,'speaking':(v.activity/1000)},ignore_index=True)
     vad_df.timestamp = pd.to_datetime(vad_df.timestamp)
     vad_df['timestamp'] = vad_df['timestamp'].dt.tz_convert('Europe/Helsinki')
+    vad_df['timestamp'] = vad_df['timestamp'].dt.tz_localize(None)
     return vad_df
 
 
