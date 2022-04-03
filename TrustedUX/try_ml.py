e2b67740d81360a6134fac66988dbb2787ae7084
Pankaj Chejara <pankajchejara23@gmail.com>
2022-04-03 19:45:32 +0200

    try script

--- TrustedUX/try_ml.py
+++ TrustedUX/try_ml.py
@@ -6,6 +6,8 @@ import numpy as np
 
 MODEL_CO = keras.models.load_model('model_CO.h5')
 
+file_name = "/home/cotrack/CoTrack-Web-mvps/media/127_2_3.png"
+
 rimg = load_img(file_name,target_size=(72,185))
 new_X = (img_to_array(rimg))
 n = new_X.reshape((1,72,185,3))
