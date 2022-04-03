86201584f4fe1cd52cb7b16a204c73a423272ca3
Pankaj Chejara <pankajchejara23@gmail.com>
2022-04-03 19:41:59 +0200

    try script

---
+++ TrustedUX/try_ml.py
@@ -0,0 +1,13 @@
+from tensorflow import keras
+import tensorflow_addons as tfa
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+import numpy as np
+
+MODEL_CO = keras.models.load_model('model_CO.h5')
+
+rimg = load_img(file_name,target_size=(72,185))
+new_X = (img_to_array(rimg))
+n = new_X.reshape((1,72,185,3))
+
+print(MODEL_CO.predict(n))
